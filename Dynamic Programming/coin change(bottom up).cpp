1d613166b1fde5bd11d6142a6934fa9b77dc7fe3
shadman_shihab <33175089+ShadmanShihab@users.noreply.github.com>
2019-03-06 00:42:07 +0600

    Add files via upload

---
+++ Dynamic Programming/coin change(bottom up).cpp
@@ -0,0 +1,27 @@
+//coin change(bottom up)
+//o(mn)
+//uva 624
+#include <bits/stdc++.h>
+using namespace std;
+
+#define Max 7495
+
+int coin[5] = {50,25,10, 5, 1};
+int make, n=5;
+int dp[Max];
+
+int main()
+{
+    memset(dp, 0, sizeof(dp));
+    dp[0] = 1;
+
+    for(int i=0; i<n; i++)
+        for(int j=coin[i], k=0; j<Max; j++, k++)
+            dp[j] += dp[k];
+        //dp[j] = dp[j] = dp[j - coin[i]];
+
+    while(scanf("%d", &make) == 1)
+        printf("%d\n", dp[make]);
+
+    return 0;
+}
