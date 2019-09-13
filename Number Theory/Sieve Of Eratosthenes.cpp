95f4313d26cc73b7f40b06b12fe68cca8706f68b
shadman_shihab <33175089+ShadmanShihab@users.noreply.github.com>
2019-09-14 00:51:24 +0600

    Update Sieve Of Eratosthenes.cpp

--- Number Theory/Sieve Of Eratosthenes.cpp
+++ Number Theory/Sieve Of Eratosthenes.cpp
@@ -33,6 +33,31 @@ void sieve(int n)
     }
 }
 
+
+/*
+void sieve()    
+{
+    bool isPrime[MAX];
+
+    for (int i = 0; i < MAX; ++i) isPrime[i] = true;
+
+    for (int i = 3; i * i <= MAX; i += 2)
+    {
+        if (isPrime[i])
+        {
+            for (int j = i * i; j <= MAX; j += i)
+            {
+                isPrime[j] = false;
+            }
+        }
+    }
+    primes.push_back(2);
+    for (int i = 3; i < MAX; i += 2) {
+        if (isPrime[i]) primes.push_back(i);
+    }
+}
+
+*/
 int main()
 {
     sieve(10000);
