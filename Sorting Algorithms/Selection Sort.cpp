793153cca76c5c0c50047d101f2fb159e77ae045
Shadman Shihab <33175089+ShadmanShihab@users.noreply.github.com>
2020-11-15 01:19:07 +0600

    Update Selection Sort.cpp

--- Sorting Algorithms/Selection Sort.cpp
+++ Sorting Algorithms/Selection Sort.cpp
@@ -6,12 +6,28 @@ using namespace std;
 int num[1000005];
 int n;
 
+/*
 void selection_sort()
 {
     for(int i=1; i<=n; i++)
         for(int j=i+1; j<=n; j++)
             if(num[i] > num[j])
                 swap(num[i], num[j]);
+}
+*/
+void selection_sort()
+{
+    int index_min;
+    for(int i=1; i<=n; i++)
+    {
+        index_min = i;
+        for(int j=i+1; j<=n; j++)
+        {
+            if(ara[j] < ara[index_min])
+                index_min = j;
+        }
+        swap(ara[i], ara[index_min]);
+    }
 }
 
 int main()
